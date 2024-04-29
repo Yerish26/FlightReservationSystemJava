6ffb3b886a12ff4c5fbb7c383ccf2e4c64c1b9c0
Yerish26 <yeghiaa@gmail.com>
2024-04-29 22:45:05 +0400

    Bug in AirportJpaPersistenceManager fixed

--- src/main/java/com/aua/flightreservationsystem/persistence/repository/airport/AirportRepository.java
+++ src/main/java/com/aua/flightreservationsystem/persistence/repository/airport/AirportRepository.java
@@ -1,9 +1,10 @@
 package com.aua.flightreservationsystem.persistence.repository.airport;
 
 import com.aua.flightreservationsystem.core.airport.Airport;
+import com.aua.flightreservationsystem.persistence.model.AirportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface AirportRepository extends JpaRepository<Airport, UUID> {
+public interface AirportRepository extends JpaRepository<AirportEntity, UUID> {
 }
