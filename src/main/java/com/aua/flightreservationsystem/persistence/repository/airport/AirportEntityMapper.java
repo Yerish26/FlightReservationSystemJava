c480982875b49eb03fa4e0d96b3c4a725aaea835
Mgrdich <mgotm13@gmail.com>
2024-04-30 23:25:26 +0400

    replace entity mappers with mapstruct

--- src/main/java/com/aua/flightreservationsystem/persistence/repository/airport/AirportEntityMapper.java
+++ src/main/java/com/aua/flightreservationsystem/persistence/repository/airport/AirportEntityMapper.java
@@ -2,29 +2,10 @@ package com.aua.flightreservationsystem.persistence.repository.airport;
 
 import com.aua.flightreservationsystem.core.airport.Airport;
 import com.aua.flightreservationsystem.persistence.model.AirportEntity;
-import com.aua.flightreservationsystem.persistence.model.CityEntity;
-import com.aua.flightreservationsystem.persistence.repository.city.CityEntityMapper;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class AirportEntityMapper {
-    private final CityEntityMapper cityEntityMapper;
-
-    public AirportEntityMapper(CityEntityMapper cityEntityMapper) {
-        this.cityEntityMapper = cityEntityMapper;
-    }
-
-    public Airport map(AirportEntity airportEntity) {
-        return Airport.builder()
-                .id(airportEntity.getId())
-                .city(cityEntityMapper.map(airportEntity.getCity()))
-                .build();
-    }
-
-    public AirportEntity map(Airport airport) {
-        AirportEntity airportEntity = new AirportEntity();
-        airportEntity.setId(airport.getId());
-        airportEntity.setCity(cityEntityMapper.map(airport.getCity()));
-        return airportEntity;
-    }
+@Mapper(componentModel = "spring")
+public interface AirportEntityMapper {
+    Airport map(AirportEntity airportEntity);
+    AirportEntity map(Airport airport);
 }
--- src/main/java/com/aua/flightreservationsystem/persistence/repository/city/CityEntityMapper.java
+++ src/main/java/com/aua/flightreservationsystem/persistence/repository/city/CityEntityMapper.java
@@ -2,40 +2,10 @@ package com.aua.flightreservationsystem.persistence.repository.city;
 
 import com.aua.flightreservationsystem.core.city.City;
 import com.aua.flightreservationsystem.persistence.model.CityEntity;
-import com.aua.flightreservationsystem.persistence.repository.airport.AirportEntityMapper;
-import com.aua.flightreservationsystem.persistence.repository.country.CountryEntityMapper;
-import com.aua.flightreservationsystem.persistence.repository.flight.FlightEntityMapper;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class CityEntityMapper {
-    private final CountryEntityMapper countryEntityMapper;
-    private final AirportEntityMapper airportEntityMapper;
-    private final FlightEntityMapper flightEntityMapper;
-
-    public CityEntityMapper(CountryEntityMapper countryEntityMapper, AirportEntityMapper airportEntityMapper, FlightEntityMapper flightEntityMapper) {
-        this.countryEntityMapper = countryEntityMapper;
-        this.airportEntityMapper = airportEntityMapper;
-        this.flightEntityMapper = flightEntityMapper;
-    }
-
-    public City map(CityEntity cityEntity) {
-        return City.builder()
-                .id(cityEntity.getId())
-                .name(cityEntity.getName())
-                .country(countryEntityMapper.map(cityEntity.getCountry()))
-                .airport(airportEntityMapper.map(cityEntity.getAirport()))
-                .flights(cityEntity.getFlights().stream().map(flightEntityMapper::map).toList())
-                .build();
-    }
-
-    public CityEntity map(City city) {
-        CityEntity cityEntity = new CityEntity();
-        cityEntity.setId(city.getId());
-        cityEntity.setName(city.getName());
-        cityEntity.setCountry(countryEntityMapper.map(city.getCountry()));
-        cityEntity.setAirport(airportEntityMapper.map(city.getAirport()));
-        cityEntity.setFlights(city.getFlights().stream().map(flightEntityMapper::map).toList());
-        return cityEntity;
-    }
+@Mapper(componentModel = "spring")
+public interface CityEntityMapper {
+    City map(CityEntity cityEntity);
+    CityEntity map(City city);
 }
