7c9f480ef4cb0f787c4aff5eded0ee3806c18356
Mgrdich <mgotm13@gmail.com>
2024-05-01 20:30:36 +0400

    Mappers formatting minor code fixes

--- src/main/java/com/aua/flightreservationsystem/persistence/repository/airline/AirlineEntityMapper.java
+++ src/main/java/com/aua/flightreservationsystem/persistence/repository/airline/AirlineEntityMapper.java
@@ -1,4 +1,5 @@
 package com.aua.flightreservationsystem.persistence.repository.airline;
+
 import com.aua.flightreservationsystem.core.airline.Airline;
 import com.aua.flightreservationsystem.persistence.model.AirlineEntity;
 import org.mapstruct.Mapper;
