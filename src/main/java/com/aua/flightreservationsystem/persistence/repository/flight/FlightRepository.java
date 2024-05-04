d594df800d9d385d85370ac9eb56ec714e742546
Yerish26 <yeghiaa@gmail.com>
2024-05-04 23:36:25 +0400

    remove FlightRepository redundant method

--- src/main/java/com/aua/flightreservationsystem/persistence/repository/flight/FlightRepository.java
+++ src/main/java/com/aua/flightreservationsystem/persistence/repository/flight/FlightRepository.java
@@ -1,10 +1,7 @@
 package com.aua.flightreservationsystem.persistence.repository.flight;
 
 import com.aua.flightreservationsystem.persistence.model.FlightEntity;
-import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FlightRepository extends JpaRepository<FlightEntity, UUID> {
-    boolean existsByIdIn(List<UUID> ids);
-}
+public interface FlightRepository extends JpaRepository<FlightEntity, UUID> {}
