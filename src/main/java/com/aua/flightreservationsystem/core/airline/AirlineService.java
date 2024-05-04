bcbd9aebfd4db6ddbe3901bb6212b85b58cc6338
Yerish26 <101129803+Yerish26@users.noreply.github.com>
2024-05-04 23:37:48 +0400

    fix AirlineService and AirlineController (#12)
    * fix AirlineService and AirlineController
    
    * remove FlightRepository redundant method

--- src/main/java/com/aua/flightreservationsystem/core/airline/AirlineService.java
+++ src/main/java/com/aua/flightreservationsystem/core/airline/AirlineService.java
@@ -1,21 +1,21 @@
 package com.aua.flightreservationsystem.core.airline;
 
-import com.aua.flightreservationsystem.core.airline.exceptions.AirlineAlreadyExistsException;
+import com.aua.flightreservationsystem.api.airline.AirlineRequest;
+import com.aua.flightreservationsystem.core.flight.Flight;
+import com.aua.flightreservationsystem.core.flight.exceptions.OneOrMoreFlightsNotFoundException;
 import com.aua.flightreservationsystem.persistence.repository.airline.AirlinePersistenceManager;
+import com.aua.flightreservationsystem.persistence.repository.flight.FlightPersistenceManager;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AirlineService {
     private final AirlinePersistenceManager airlinePersistenceManager;
-
-    @Autowired
-    public AirlineService(AirlinePersistenceManager airlinePersistenceManager) {
-        this.airlinePersistenceManager = airlinePersistenceManager;
-    }
+    private final FlightPersistenceManager flightPersistenceManager;
 
     public List<Airline> getAll() {
         return airlinePersistenceManager.findAll();
@@ -25,15 +25,40 @@ public class AirlineService {
         return airlinePersistenceManager.findById(id);
     }
 
-    public Airline save(Airline airline) throws AirlineAlreadyExistsException {
-        if (airlinePersistenceManager.findById(airline.getId()).isPresent()) {
-            throw new AirlineAlreadyExistsException(airline.getId());
-        }
+    public Airline save(AirlineRequest airlineRequest) throws OneOrMoreFlightsNotFoundException {
+
+        List<Flight> flights = getFlights(airlineRequest);
+
+        Airline airline = getAirline(airlineRequest, flights);
         return airlinePersistenceManager.save(airline);
     }
 
-    public Airline update(Airline airline) {
-        return airlinePersistenceManager.save(airline);
+    private static Airline getAirline(AirlineRequest airlineRequest, List<Flight> flights) {
+        return Airline.builder().name(airlineRequest.getName()).flights(flights).build();
+    }
+
+    private List<Flight> getFlights(AirlineRequest airlineRequest) throws OneOrMoreFlightsNotFoundException {
+        List<Flight> flights = flightPersistenceManager.findAllById(airlineRequest.getFlightIds());
+
+        if (flights.size() != airlineRequest.getFlightIds().size()) {
+            throw new OneOrMoreFlightsNotFoundException("One or more flights were not found.");
+        }
+        return flights;
+    }
+
+    public Airline update(UUID id, AirlineRequest airlineRequest) throws OneOrMoreFlightsNotFoundException {
+        List<Flight> flights = getFlights(airlineRequest);
+
+        Optional<Airline> existingAirlineOptional = airlinePersistenceManager.findById(id);
+
+        if (existingAirlineOptional.isPresent()) {
+            Airline existingAirline = existingAirlineOptional.get();
+            Airline updatedAirline = getAirline(airlineRequest, flights).toBuilder()
+                    .id(existingAirline.getId())
+                    .build();
+            return airlinePersistenceManager.save(updatedAirline);
+        }
+        return save(airlineRequest);
     }
 
     public void delete(UUID id) {
